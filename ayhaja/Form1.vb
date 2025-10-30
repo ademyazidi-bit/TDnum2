9d39add17e101ed40b840e6be756f08070c1035a
ademyazidi-bit <adem.yazidi@isgb.ucar.tn>
2025-10-30 09:18:17 +0100

    bads

---
+++ ayhaja/Form1.vb
@@ -0,0 +1,5 @@
+﻿Public Class Form1
+    Private Sub frmAjoutPatient(sender As Object, e As EventArgs) Handles MyBase.Load
+        lblDate.Text = Date.Today.ToLongDateString()
+    End Sub
+End Class
---
+++ ayhaja/Patients.vb
@@ -0,0 +1,14 @@
+﻿Module Patients
+    Public Structure Patient
+        Dim code As Integer
+        Dim nom As String
+        Dim prenom As String
+        Dim age As Integer
+    End Structure
+    Public listePatients As List(Of Patient)
+    Public Function AjouterPAtient(p)
+        p.code = listePatients.Count
+
+
+    End Function
+End Module
