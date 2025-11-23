1bde6bbf84fc1247a0092a01fb0b23155e1596de
ademyazidi-bit <adem.yazidi@isgb.ucar.tn>
2025-11-23 11:57:24 +0100

    new

--- Nettoyage/Articles.vb
+++ Nettoyage/Articles.vb
@@ -1,6 +1,5 @@
 ﻿Module Articles
 
-
     Public Structure Article
         Dim code As String
         Dim vetement As String
@@ -10,14 +9,17 @@
 
         Public Sub initArticle()
             supplement = New List(Of Boolean)({False, False})
-
         End Sub
     End Structure
+
     Public listeArticles As New List(Of Article)
 
-
     Public Sub AjouterArticle(A As Article)
-        A.code = A.vetement.Substring(1, 2) & "_" & listeArticles.Count
+        If A.vetement.Length >= 3 Then
+            A.code = A.vetement.Substring(1, 2) & "_" & listeArticles.Count
+        Else
+            A.code = A.vetement & "_" & listeArticles.Count
+        End If
         listeArticles.Add(A)
     End Sub
 End Module
--- Nettoyage/Form1.vb
+++ Nettoyage/Form1.vb
@@ -2,30 +2,26 @@
     Dim chsupp As String
 
     Private Sub RadioButton2_CheckedChanged(sender As Object, e As EventArgs) Handles RbPan.CheckedChanged
-
     End Sub
 
     Private Sub RadioButton3_CheckedChanged(sender As Object, e As EventArgs) Handles RbChemise.CheckedChanged
-
     End Sub
 
     Private Sub RadioButton6_CheckedChanged(sender As Object, e As EventArgs) Handles RbRepa.CheckedChanged
-
     End Sub
 
     Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
-
     End Sub
 
     Private Sub DataGridView1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellContentClick
-
     End Sub
 
     Private Sub BrnAjou_Click(sender As Object, e As EventArgs) Handles BrnAjou.Click
         Dim article As Article
         Dim test As Boolean = False
+
         For Each ctrl In GroupBoxVet.Controls
-            If ctrl.Checked Then
+            If TypeOf ctrl Is RadioButton AndAlso CType(ctrl, RadioButton).Checked Then
                 article.vetement = ctrl.text
                 test = True
             End If
@@ -34,10 +30,11 @@
             ErrorProvider1.SetError(GroupBoxVet, "veillez choisir un vetemen : ")
             Return
         End If
+
         test = False
         For Each ctrl In GroupBoxNett.Controls
-            If ctrl.Checked Then
-                article.vetement = ctrl.text
+            If TypeOf ctrl Is RadioButton AndAlso CType(ctrl, RadioButton).Checked Then
+                article.typeNettoyage = ctrl.text
                 test = True
             End If
         Next
@@ -45,33 +42,43 @@
             ErrorProvider1.SetError(GroupBoxNett, "veillez choisir un vetement: ")
             Return
         End If
+
         Dim nb As Integer
         nb = CInt(NumericUpDownArticle.Value)
         If nb = 0 Then
             ErrorProvider1.SetError(GroupBoxNett, "veillez Donner un nombre  > 0  : ")
             Return
-
         End If
+
         article.nombre = nb
         article.initArticle()
+
         If ChDef.Checked Then
             article.supplement(0) = True
-            GroupBoxSupp = v
-
         End If
         If ChAnti.Checked Then
             article.supplement(1) = True
-
         End If
+
         AjouterArticle(article)
-        'init
         MAJ_DGV()
     End Sub
-    Private Sub MAJ_DGV()
 
+    Private Sub MAJ_DGV()
+        DataGridView1.Rows.Clear()
+        For Each I In listeArticles
+            Dim chsupp As String = ""
+            If I.supplement(0) = True Then
+                chsupp = "Defroissage "
+            End If
+            If I.supplement(1) = True Then
+                chsupp = "Anti Tache"
+
+            End If
+            DataGridView1.Rows.Add(I.code, I.vetement, I.typeNettoyage, I.nombre, chsupp)
+        Next
     End Sub
 
     Private Sub ChAnti_CheckedChanged(sender As Object, e As EventArgs) Handles ChAnti.CheckedChanged
-
     End Sub
 End Class
